a6074525d595082e7cfc91fe18fdcff8e102a1b7
giovcavaliere <giov.cavaliere@gmail.com>
2026-08-12 09:50:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='diario-v2.5';
-const FILES=['./','./index.html','./style.css?v=2.5','./app.js?v=2.5','./manifest.json'];
+const CACHE='diario-v2.6.1';
+const FILES=['./','./index.html','./style.css?v=2.6.1','./app.js?v=2.6.1','./manifest.json'];
 
 self.addEventListener('install',e=>{
   self.skipWaiting();
